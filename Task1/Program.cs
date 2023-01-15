2c38d91d7162fd671747d7df29231720174341cf
Darya <iontseva.dasha>
2023-01-15 13:23:08 +0300

    first commit

---
+++ Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдает, какое число большее,
+//  а какое меньшее
+// a=5 b=7 -> max=7, min=5
+// a=-9 b=-3 -> max=-3, min=-9
+
+Console.WriteLine("Введите целое число > ");
+
+string? value1 = Console.ReadLine();
+int a = Convert.ToInt32(value1);
+
+Console.WriteLine("Введите целое число > ");
+
+string? value2 = Console.ReadLine();
+int b = Convert.ToInt32(value2);
+
+if (a > b) 
+{
+    Console.WriteLine($"Число {a} является максимальным, число {b} является минимальным");
+}
+
+else
+{
+    Console.WriteLine($"Число {a} является минимальным, число {b} является максимальным");
+}
+
+
---
+++ Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдает максимальное из этих чисел
+// 2,3,7 -> max=7
+
+Console.WriteLine("Введите целое число > ");
+string? value1 = Console.ReadLine();
+int a = Convert.ToInt32(value1);
+
+Console.WriteLine("Введите целое число > ");
+string? value2 = Console.ReadLine();
+int b = Convert.ToInt32(value2);
+
+Console.WriteLine("Введите целое число > ");
+string? value3 = Console.ReadLine();
+int c = Convert.ToInt32(value3);
+
+if ((a > b) && (a > c))
+{
+    Console.WriteLine($"Максимальное число {a}");
+}
+
+if ((b > a) && (b > c))
+{
+    Console.WriteLine($"Максимальное число {b}"); 
+}
+if ((c > a) && (c > b))
+{
+    Console.WriteLine($"Максимальное число {c}");
+}
+
